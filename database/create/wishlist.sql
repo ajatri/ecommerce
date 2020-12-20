975f149fdd95972a809f34261a81c55b842da624
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 12:44:51 +0530

    Create wishlist.sql

---
+++ database/create/wishlist.sql
@@ -0,0 +1,7 @@
+create table if not exists wishlist(
+    wid BIGINT PRIMARY KEY COMMENT "Unique Id for the wishlist",
+    pid BIGINT NOT NULL ,
+    uid BIGINT NOT NULL ,
+    FOREIGN KEY (pid) REFERENCES Product(pid)
+    FOREIGN KEY (uid) REFERENCES user(uid)
+    );
