e82aa3f5834107bcf0da1275f04529eac7415c5b
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 13:58:09 +0530

    Update product.sql

--- database/create/product.sql
+++ database/create/product.sql
@@ -1,7 +1,7 @@
 create table if not exists product(
-    pid BIGINT PRIMARY KEY COMMENT "Unique Id for the Product",
-    p_name VARCHAR(30) NOT NULL ,
-      p_price INT NOT NULL ,
-    p_image VARCHAR(200) NOT NULL,
-    p_desc varchar(200) NOT NULL
+    id BIGINT PRIMARY KEY COMMENT "Unique Id for the Product",
+    name VARCHAR(30) NOT NULL ,
+      price INT NOT NULL ,
+    image VARCHAR(200) NOT NULL,
+    desc varchar(200) NOT NULL
   );
