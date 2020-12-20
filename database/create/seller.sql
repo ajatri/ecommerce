2289e240982c261ae5358b80bf89dd39bed91ddb
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 20:13:45 +0530

    Update seller.sql

--- database/create/seller.sql
+++ database/create/seller.sql
@@ -1,7 +1,7 @@
 create table if not exists seller(
     id BIGINT PRIMARY KEY COMMENT "Unique Id for the Seller",
-    name VARCHAR(30) NOT NULL ,
-      email VARCHAR NOT NULL ,
-    phone INT NOT NULL,
-    address varchar(200) NOT NULL
+    name VARCHAR(30) NOT NULL COMMENT "name of  Seller" ,
+      email VARCHAR NOT NULL COMMENT "email Seller",
+    phone INT NOT NULL COMMENT "Phone number of Seller",
+    address varchar(200) NOT NULL COMMENT "address of the seller"
   );
