bdf0bcb3e6d375873e389b572a057017a00d34b7
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 14:15:58 +0530

    Update psmapping.sql

--- database/create/psmapping.sql
+++ database/create/psmapping.sql
@@ -1,6 +1,7 @@
 create table if not exists psmapping(
-    pid BIGINT PRIMARY KEY COMMENT "Unique Id for the product-sller mapping",
+    id BIGINT PRIMARY KEY COMMENT "Unique Id for the product-sller mapping",
+    pid BIGINT NOT NULL,
     sid BIGINT NOT NULL ,
-    FOREIGN KEY (pid) REFERENCES product(pid)
-    FOREIGN KEY (sid) REFERENCES seller(sid)
+    FOREIGN KEY (pid) REFERENCES product(id)
+    FOREIGN KEY (sid) REFERENCES seller(id)
     );
