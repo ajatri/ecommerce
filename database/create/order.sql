4e7d236a6023a2f41c56effcf5f0dd0b50592035
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 20:49:14 +0530

    Update order.sql

--- database/create/order.sql
+++ database/create/order.sql
@@ -1,7 +1,7 @@
 create table if not exists order(
     id BIGINT PRIMARY KEY COMMENT "Unique Id for the order ",
-    uid BIGINT NOT NULL ,
-    shipid INT NOT NULL ,
+    uid BIGINT NOT NULL  COMMENT " Id of the user ",
+    shipid INT NOT NULL  COMMENT " Id for the shipper " ,
     FOREIGN KEY(uid) REFERENCES user(id),
    FOREIGN KEY(shipid) REFERENCES shipper(id)
   );
