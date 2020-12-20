be49bc7b5665a4c86d5e47118b952e80bb6007f0
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 19:42:17 +0530

    Update shipper.sql

--- database/create/shipper.sql
+++ database/create/shipper.sql
@@ -1,7 +1,7 @@
 create table if not exists shipper(
-    shipid BIGINT PRIMARY KEY COMMENT "Unique Id for the shipper",
-    ship_name VARCHAR(30) NOT NULL ,
-      ship_phone INT NOT NULL ,
-    ship_address VARCHAR(200) NOT NULL,
-    ship_email VARCHAR(30)  NOT NULL
+   id BIGINT PRIMARY KEY COMMENT "Unique Id for the shipper",
+   name VARCHAR(30) NOT NULL ,
+      phone INT NOT NULL ,
+    address VARCHAR(200) NOT NULL,
+   email VARCHAR(30)  NOT NULL
   );
