1eb266d8190038fef1416bc7f1fc2b7765053a53
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 20:07:57 +0530

    Update address.sql

--- database/create/address.sql
+++ database/create/address.sql
@@ -1,10 +1,10 @@
   create table if not exists address(
     id BIGINT PRIMARY KEY COMMENT "Unique Id of the user",
-    landmark VARCHAR(100) "any object or feature to identify the location "
-    city VARCHAR(30) NOT NULL "city/village of the user",
-      state VARCHAR(30) NOT NULL "state of the user" ,
-    email VARCHAR(30) NOT NULL "email of user",
-    phone INT  NOT NULL " Phone number of the user",
-    pin INT  NOT NULL "pin code ",
+    landmark VARCHAR(100) COMMENT "any object or feature to identify the location "
+    city VARCHAR(30) NOT NULL COMMENT "city/village of the user",
+      state VARCHAR(30) NOT NULL COMMENT "state of the user" ,
+    email VARCHAR(30) NOT NULL  COMMENT"email of user",
+    phone INT  NOT NULL  COMMENT" Phone number of the user",
+    pin INT  NOT NULL  COMMENT"pin code ",
     FOREIGN KEY (id) REFERENCES user(id)
   );
