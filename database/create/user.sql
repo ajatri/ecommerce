5b098ca6260e68548cda0d982d936f813716a1c8
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-20 10:10:05 +0530

    Update user.sql

--- database/create/user.sql
+++ database/create/user.sql
@@ -1,7 +1,6 @@
 create table if not exists user(
     uid BIGINT PRIMARY KEY COMMENT "Unique Id for the User",
     first_name VARCHAR(30) NOT NULL COMMENT "First name of the User/Customer",
-    middle_name VARCHAR(30),
-    last_name VARCHAR(30),
-    dob DATETIME,
-);
+    u_email VARCHAR(30) ,
+    password VARCHAR(30)
+  );
