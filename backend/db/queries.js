a8e43bf6765782dd41cc9d173f0d8501b82ba725
ajatri <49090914+ajatri@users.noreply.github.com>
2020-12-22 13:05:07 +0530

    Update queries.js

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -2,5 +2,11 @@ module.exports = {
     AddSeller: "insert into seller(name,email,phone,address) values (?,?,?,?);",
     GetAllSellers: "select * from seller;",
     UpdateSeller: "update seller set name=?,email=?,phone=?,address=? where id=?",
-    DeleteSeller: "delete from seller where id=?"
+    DeleteSeller: "delete from seller where id=?",
+    
+    AddProducts: "insert into product(name,price,image,des) values (?,?,?,?);",
+    GetAllProdcts: "select * from product;",
+    UpdateProduct: "update product set name=?,price=?,image=?,des=? where id=?",
+    DeleteProduct: "delete from product where id=?"
+}
 }
