57c518d1c7bdf655d45abddf0c4793be6d5c222f
Ajatri <aparna2014.a@gmail.com>
2020-12-23 21:53:30 +0530

    update user

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -6,5 +6,9 @@ module.exports = {
      AddProduct: "insert into product(name,price,image,des) values (?,?,?,?);",
     GetAllProdcts: "select * from product;",
     UpdateProduct: "update product set name=?,price=?,image=?,des=? where id=?;",
-    DeleteProduct: "delete from product where id=?;"
+    DeleteProduct: "delete from product where id=?;",
+    AddUser: "insert into user(first_name,last_price,email,pass) values (?,?,?,?);",
+    GetAllUsers: "select * from user;",
+    UpdateUser: "update user set first_name=?,last_name=?,email=?,pass=? where id=?;",
+    DeleteUser: "delete from user where id=?;"
 }
