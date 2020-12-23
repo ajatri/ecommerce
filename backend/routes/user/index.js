f360010b617852a86d1b3d5a52a23d4d2beba534
Ajatri <aparna2014.a@gmail.com>
2020-12-23 21:53:56 +0530

    update user CRUD

--- backend/routes/user/index.js
+++ backend/routes/user/index.js
@@ -1,10 +1,48 @@
+const getConnection = require("../../db");
+const queries = require("../../db/queries")
+
 const router = require("express").Router();
 
-router.get("/",(req,res)=>{
-    res.send([{
-            name:"Rakshith",
-            age:100
-        }]);
+//CRUD-Create Read Update Delete
+
+//Create
+router.post("/", async (req, res) => {
+    const { body } = req;
+    getConnection()
+        .then(connection => connection.execute(queries.AddUser, [body.first_name, body.last_name, body.email, body.pass])
+            .then(result => { res.status(200).end(JSON.stringify({ message: { result: result[0] } })) })
+            .catch(error => { res.status(500).end(JSON.stringify({ error })) }))
+        .catch(console.log);
+});
+
+//Read
+router.get("/", async (req, res) => {
+    getConnection()    
+        .then(connection => connection.query(queries.GetAllUsers)
+            .then(result => { res.status(200).end(JSON.stringify({ users : { result: result[0] } })) })
+            .catch(error => { res.status(500).end(JSON.stringify({ error })) })
+        )
+        .catch(console.log)
+    //add more logic in future to get product by id
+});
+
+//Update
+router.put("/", async (req, res) => {
+    const { body } = req;
+    getConnection()
+        .then(connection => connection.execute(queries.UpdateUser, [body.first_name, body.last_name, body.email, body.pass, body.id])
+            .then(result => { res.status(200).end(JSON.stringify({ message: { result: result[0] } })) })
+            .catch(error => { res.status(500).end(JSON.stringify({ error })) }))
+        .catch(console.log);
 })
 
-module.exports=router;
+//delete
+router.delete("/", async (req, res) => {
+    const { body } = req;
+    getConnection()
+        .then(connection => connection.execute(queries.DeleteUser, [body.id])
+            .then(result => { res.status(200).end(JSON.stringify({ message: { result: result[0] } })) })
+            .catch(error => { console.log(error); res.status(500).end(JSON.stringify({ error })) }))
+        .catch(console.log);
+});
+module.exports = router;
