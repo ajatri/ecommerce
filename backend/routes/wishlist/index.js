8b872c31ad272a6a7981ed7844c02fe2e8c4d97a
Ajatri <aparna2014.a@gmail.com>
2021-01-12 12:11:55 +0530

    updated

--- backend/routes/wishlist/index.js
+++ backend/routes/wishlist/index.js
@@ -19,30 +19,6 @@ console.log('bye')
 
 
 
-router.get("/check", async(req, res) => {
-    try {
-        const pid = req.body.pid;
-const uid = req.userDetails.id;
-        getConnection()
-        .then(connection => connection.execute(queries.Check, [uid,pid]))
-        .then(result => {       
-              result=result[0];
-              console.log(result,'djfhhrhfksf');
-	            if(result.length>0){
-                    const payload = { username, password, id: result[0].id };
-  	          	const token = jwt.sign(payload, 'secretkey', { expiresIn: 60 * 60 });
-    	        	res.status(200).json({ token});
-              }else{
-	              res.status(403).json({error:"user not found"});
-              }
-        })
-        .catch((error)=>{res.status(500).json({ error })});
-    } catch (error) {
-      console.log(error.message);
-        res.status(500).json({ error });
-    }
-
-});
 
 
 
